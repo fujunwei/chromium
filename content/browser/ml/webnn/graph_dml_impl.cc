700a9574a83804c66079a6973c8021d5c9cf0b37
junwei <junwei.fu@intel.com>
2022-07-03 10:24:41 +0800

    WebNN: a single big CL to prototype GPU backend
    Implement WebNN GPU backend in the GPU process with a singleton service which
    communicates with render process with mojo infrastructure. There are a WireServer
    in GPU and WireClient in render process for every navigator.ml. The Object ID of
    each IDL interface is used to refer to this object when talking with the server side.
    
    This prototype supports 8 operations required by MobileNet V2 [1] for performance
    evaluation purpose. The input and output data use shared buffer across process.
    WebNN DirectML is about 260% faster than WebGL on Windows platform.
    
    The CL is based on the patchset 28 of XNNPACK implementation [2].
    
    [1]: https://arxiv.org/abs/1801.04381
    [2]: https://chromium-review.googlesource.com/c/chromium/src/+/3684745
    
    Bug: 1273291

---
+++ content/browser/ml/webnn/graph_dml_impl.cc
@@ -0,0 +1,117 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/ml/webnn/graph_dml_impl.h"
+
+#include "base/memory/ptr_util.h"
+#include "mojo/public/cpp/bindings/self_owned_receiver.h"
+
+namespace content::webnn {
+
+namespace {
+
+using ml::webnn::mojom::BuildResult;
+using ml::webnn::mojom::ComputeResult;
+using ml::webnn::mojom::Graph;
+
+}  // namespace
+
+// static
+void GraphDMLImpl::Create(mojo::PendingReceiver<Graph> receiver) {
+  mojo::MakeSelfOwnedReceiver<Graph>(base::WrapUnique(new GraphDMLImpl()),
+                                     std::move(receiver));
+}
+
+GraphDMLImpl::~GraphDMLImpl() = default;
+
+GraphDMLImpl::GraphDMLImpl() = default;
+
+void GraphDMLImpl::AddInput(const std::string& name,
+                            OperandDescriptorPtr desc) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddConstant(OperandDescriptorPtr desc,
+                               const std::vector<uint8_t>& array_buffer) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddElementWiseBinary(uint32_t a_id,
+                                        uint32_t b_id,
+                                        BinaryOperandType type,
+                                        OperandDescriptorPtr output_desc) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddClamp(uint32_t input_id,
+                            ClampOptionsPtr options,
+                            OperandDescriptorPtr output_desc) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddConv2d(uint32_t input_id,
+                             uint32_t filter_id,
+                             Conv2dOptionsPtr options,
+                             OperandDescriptorPtr output_desc) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddReshape(uint32_t input_id,
+                              OperandDescriptorPtr output_desc) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddGemm(uint32_t a_id,
+                           uint32_t b_id,
+                           GemmOptionsPtr options,
+                           OperandDescriptorPtr output_desc) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddPool2d(uint32_t input_id,
+                             Pool2dOptionsPtr options,
+                             Pool2dType type,
+                             OperandDescriptorPtr output_desc) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddUnary(uint32_t input_id,
+                            UnaryOperandType type,
+                            OperandDescriptorPtr output_desc) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::AddFusionClamp(ClampOptionsPtr options,
+                                  uint32_t operator_id) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+}
+
+void GraphDMLImpl::Build(
+    const base::flat_map<std::string, uint32_t>& named_operands,
+    BuildCallback callback) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+  std::move(callback).Run(BuildResult::kUnknownError);
+}
+
+void GraphDMLImpl::Compute(NamedInputsPtr named_inputs,
+                           ComputeCallback callback) {
+  // TODO(crbug.com/1273291): Implement this with DirectML.
+  NOTIMPLEMENTED();
+  auto named_outputs = ml::webnn::mojom::NamedOutputs::New();
+  std::move(callback).Run(ComputeResult::kUnknownError,
+                          std::move(named_outputs));
+}
+
+}  // namespace content::webnn
